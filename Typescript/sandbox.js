a23feb26c5e877ea96bab07c6cb576c8f89302ad
TiokengSamuel <samueledward40@gmail.com>
2021-03-09 22:58:16 +0100

    sandbox

--- Typescript/sandbox.js
+++ Typescript/sandbox.js
@@ -1,4 +1,7 @@
-var character = 'mario';
+var character = 'Luigi';
 console.log(character);
 var inputs = document.querySelectorAll('input');
 console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
--- Typescript/sandbox.ts
+++ Typescript/sandbox.ts
@@ -1,7 +1,11 @@
-const character = 'mario'
+const character = 'Luigi'
 
 console.log(character);
 
 const inputs = document.querySelectorAll('input');
 
 console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input);
+})
